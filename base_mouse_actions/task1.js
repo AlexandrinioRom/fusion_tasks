d69c717df64f1c3125c0c322ed83bb633e515962
AlexandrinioRom <funny13soul@gmail.com>
2021-11-06 20:49:51 +0300

    add base mouse action task

---
+++ base_mouse_actions/task1.js
@@ -0,0 +1,16 @@
+
+ul.onclick = function (event) {
+    const target = event.target;
+
+    if (event.metaKey || event.ctrlKey) {
+        target.classList.add('selected');
+    } else {
+        for (let elem of ul.children) {
+            elem.classList.remove('selected')
+            if (elem = target) {
+                target.classList.add('selected')
+            }
+        }
+    }
+}
+ul.onmousedown = (event) => false;
