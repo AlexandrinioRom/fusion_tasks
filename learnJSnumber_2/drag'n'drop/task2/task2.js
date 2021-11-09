2ab2a6e7c5a7b65149fe91390a0f8b856fcc9d62
AlexandrinioRom <funny13soul@gmail.com>
2021-11-09 23:54:17 +0300

    add task2

--- learnJSnumber_2/drag'n'drop/task2/task2.js
+++ learnJSnumber_2/drag'n'drop/task2/task2.js
@@ -1,10 +1,12 @@
+'use strict'
+
 document.addEventListener('mousedown', onMouseDown);
+document.ondragstart = () => false;
 
 function onMouseDown(event) {
 
     const target = event.target;
     if (!target.closest('.draggable')) return;
-    if (target.tagName == 'IMG') target.ondragstart = () => false;
 
     const position = target.getBoundingClientRect();
     const shiftX = event.clientX - position.left;
@@ -13,23 +15,65 @@ function onMouseDown(event) {
     document.addEventListener('mousemove', onMouseMove);
     target.addEventListener('mouseup', removeHandler);
 
+    target.style.position = 'absolute';
+    target.style.zIndex = 1000;
+    document.body.append(target);
+
+    moveAt(event.clientX, event.clienty);
+
     function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
+        moveAt(event.clientX, event.clientY);
     }
 
-    function moveAt(pageX, pageY) {
-        target.style.left = pageX - shiftX + 'px';
-        target.style.top = pageY - shiftY + 'px';
+    function moveAt(clientX, clientY) {
+
+        const docElem = document.documentElement;
+        const docPosition = docElem.getBoundingClientRect();
+        const rightSide = docPosition.width - target.offsetWidth;
+        const bottomSide = docPosition.height - target.offsetHeight;
+
+        let newY = clientY - shiftY;
+        let newBottom = newY + target.offsetHeight;
+
+        let newLeft = clientX - shiftX - docPosition.left;
+        let newTop = newY - docPosition.top;
+
+        if (newLeft < 0) {
+            newLeft = 0;
+        }
+        if (newTop < 0) {
+            newTop = 0;
+        }
+        if (newLeft > rightSide) {
+            newLeft = rightSide;
+        }
+        if (newTop > bottomSide) {
+            newTop = bottomSide;
+        }
+
+        // условие с прокруткой, которое идет ниже - не мое -----------------
+
+        if (newBottom > docElem.clientHeight) {
+            let scrollY = Math.min(docPosition.bottom - newBottom, 10);
+            if (scrollY < 0) scrollY = 0;
+            window.scrollBy(0, scrollY);
+            newY = Math.min(newY, docElem.clientHeight - target.offsetHeight);
+        }
+        if (newY < 0) {
+            let scrollY = Math.min(-newY, 10);
+            if (scrollY < 0) scrollY = 0; 
+            window.scrollBy(0, -scrollY);
+            newY = Math.max(newY, 0); 
+        }
+
+        // ------------------------------------------------------------------
+
+        target.style.left = newLeft + 'px';
+        target.style.top = newTop + 'px';
     }
 
     function removeHandler () {
         document.removeEventListener('mousemove', onMouseMove);
         target.onmouseup = null;
     }
-    
-    target.style.position = 'absolute';
-    target.style.zIndex = 1000;
-    document.body.append(target);
-
-    moveAt(event.pageX, event.pageY);
 }
